ddf4281519980a5b9e13e645d9e62a92f025e38a
Neha Sharma <147707969+Neha12Sharma@users.noreply.github.com>
2025-08-12 21:25:49 +0530

    Add files via upload

---
+++ js-2/main.js
@@ -0,0 +1,70 @@
+document.write('<br> hellow javascript');
+
+document.write('<h2>javascript output <h2>');
+
+// document.write()
+// window.alert()
+// consol.log()
+// innerHTML()
+
+
+window.alert('hellow alert! ') ; 
+
+document.getElementById('text').innerHTML='Hellow Geeta University';
+
+console.log('hellow students!');
+
+document.write('<h3 align="center">javascript variable</h3>');
+
+// var,let,const
+
+document.write('<h3>javascript variable with VAR keyword</h3>');
+var a=35;
+var name1='Neha Sharma';
+var d=3.2;
+var a=79;
+a=200;
+if(a==200){
+    var f=307;
+    document.write('value F is inside if block:'+f +'<br>');
+}
+document.write('value F is outside if block:'+f + '<br>');
+
+document.write('value A is:'+a + '<br> value name is:' +name1 + '<br> value d is:' +d + '<br>value A is:'+a);
+// document.write('value A is:'+a);
+
+document.write('<h3>javascript variable with LET keyword</h3>');
+let b=45;
+
+// let b=90; redeclair not supported
+b = 48;
+document.write('value B is:'+b +'<br>');
+
+if(b==48){
+    var e=400;
+    document.write('value E is inside if block:'+e +'<br>');
+}
+document.write('value E is outside if block:'+e + '<br>');
+
+document.write('<h3>javascript variable with CONST keyword</h3>');
+const c=67;
+// const c=56; redeclair not supported
+// c=23;reassignment not supported
+document.write('value C is:'+c +'<br>');
+if(c==67){
+    const g=400;
+    document.write('value G is inside if block:'+g +'<br>');
+}
+document.write('value G is outside if block:'+g + '<br>');
+
+
+
+
+
+
+
+
+
+
+
+
